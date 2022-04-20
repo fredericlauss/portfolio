c93ae8b2cec468b3284ce3f8037212a1d073185e
lausson frederic <flausson@ftel.fr>
2022-04-20 16:57:35 +0200

    [porfolio] init admin + debut form

---
+++ admin/connexion.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Espace de connection</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="impression.css" media="print">
+</head>
+<body>
+    <form method="POST" action="">
+        <input type="text" name="speudo" autocomplete="off">
+        <input type="password" name="mdp" autocomplete="off">
+        <input type="submit" name="valider">
+    </form>
+
+<script type="text/javascript" src="script.js"></script>
+</body>
+</html>
