71931a11e09025383823ed31322ed2f63cb96aa3
lausson frederic <flausson@ftel.fr>
2022-04-20 15:12:02 +0200

    [portfolio] fin lightmode

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+
+/* light mode */
+
+
 document.querySelector('#lightmode').addEventListener('change', function(event){
     document.querySelector('body').classList.toggle('light');
   });
+
+
+
+
+/* nav ancres actif */ 
+
