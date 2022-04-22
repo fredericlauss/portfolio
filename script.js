618745b957a74479a8a4b5bd8cf44ed78625c793
lausson frederic <flausson@ftel.fr>
2022-04-22 14:55:19 +0200

    [portfolio] fix light mode

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 
 /* light mode */
 
-
+/*
 document.querySelector('#lightmode').addEventListener('change', function(event){
     document.querySelector('body').classList.toggle('light');
   });
 
+*/
+  document.querySelector('#lightmode').addEventListener( 'change', function() {
+    localStorage.setItem('light',this.checked);
+    if(this.checked) {
+      document.querySelector('body').classList.add('light');
+    } else {
+      document.querySelector('body').classList.remove('light');
+         
+    }
+});
+
+window.onload = function(a) {
+  console.log(localStorage.getItem('light'))
+
+
+  if(localStorage.getItem('light') === "true") {
+    document.querySelector('body').classList.add('light');
+  }
+};
 
 
