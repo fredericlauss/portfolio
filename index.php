05bec0518d1a755d4354da9d57dcc00e52dd2445
lausson frederic <flausson@ftel.fr>
2022-05-11 12:28:31 +0200

    [cours light house ] fin page index

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Page d'accueil de mon portfolio. Je suis developper web.">
     <!-- <meta name=”robots” content=”index,follow”> poser question ??? -->
+    <!-- https://developer.mozilla.org/fr/docs/Web/HTTP/CSP  -->
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" type="text/css" href="impression.css" media="print">
 
@@ -43,7 +44,7 @@
                 <h3>test</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 <div class="div-center">
-                    <a class="btn-s" href="portfolio.php">à propos</a>
+                    <a class="btn-s" href="a-propos.php">à propos</a>
                 </div>
             </div>
         </div>
