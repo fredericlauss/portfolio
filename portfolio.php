6b0f799b91ae4c191e6e5cb9eaea7c4c62b556c0
frederic <flausson@normandiewebschool.fr>
2022-07-01 12:20:13 +0200

    [portfolio] link

--- portfolio.php
+++ portfolio.php
@@ -17,48 +17,41 @@
     <div class="section">
             <div class="wrapper">
                 <h2>mes projets</h2>
-                <h3>front</h3>
+                <h3>Sites Mon Site Vert</h3>
                 <div class="galerie">
-                <a href="portfolio.php">
+                <a href="https://www.kyoss.fr/" target="_blank">
                     <img loading="lazy"  width="250" height="250" class="galerie-enfant" id="test-galerie" src="media\11_Post-production - Copie.png" alt="marie minchella">
                     <div class="galerie-titre-p">
-                        <h4>titre</h4>
-                        <p>exemple de texte pour faire cliquer !</p>
+                        <h4>Kyoss</h4>
+                        <p>Aller directement au site</p>
                     </div>
                 </a>
-                <a href="portfolio.php">
+                <a href="https://www.neodd2030.fr/" target="_blank">
                     <img loading="lazy"  width="250" height="250" class="galerie-enfant" id="test-galerie" src="media\11_Post-production - Copie.png" alt="marie minchella">
                     <div class="galerie-titre-p">
-                        <h4>titre</h4>
-                        <p>exemple de texte pour faire cliquer !</p>
+                        <h4>NEODD</h4>
+                        <p>Aller directement au site</p>
                     </div>
                 </a>
-                <a href="portfolio.php">
+                <a href="https://www.marieminchella.com/" target="_blank">
                     <img loading="lazy"  width="250" height="250" class="galerie-enfant" id="test-galerie" src="media\11_Post-production - Copie.png" alt="marie minchella">
                     <div class="galerie-titre-p">
-                        <h4>titre</h4>
-                        <p>exemple de texte pour faire cliquer !</p>
+                        <h4>Marie Minchella</h4>
+                        <p>Aller directement au site</p>
                     </div>
                 </a>
-                <a href="portfolio.php">
+                <a href="https://www.terra-num.fr/" target="_blanl">
                     <img loading="lazy"  width="250" height="250" class="galerie-enfant" id="test-galerie" src="media\11_Post-production - Copie.png" alt="marie minchella">
                     <div class="galerie-titre-p">
-                        <h4>titre</h4>
-                        <p>exemple de texte pour faire cliquer !</p>
+                        <h4>Terra-num</h4>
+                        <p>Aller directement au site</p>
                     </div>
                 </a>
-                <a href="portfolio.php">
+                <a href="https://www.inmediavita.com/" target="_blanl">
                     <img loading="lazy"  width="250" height="250" class="galerie-enfant" id="test-galerie" src="media\11_Post-production - Copie.png" alt="marie minchella">
                     <div class="galerie-titre-p">
-                        <h4>titre</h4>
-                        <p>exemple de texte pour faire cliquer !</p>
-                    </div>
-                </a>
-                <a href="portfolio.php">
-                    <img loading="lazy"  width="250" height="250" class="galerie-enfant" id="test-galerie" src="media\11_Post-production - Copie.png" alt="marie minchella">
-                    <div class="galerie-titre-p">
-                        <h4>titre</h4>
-                        <p>exemple de texte pour faire cliquer !</p>
+                        <h4>In Media Vita</h4>
+                        <p>Aller directement au site</p>
                     </div>
                 </a>
                 </div>
